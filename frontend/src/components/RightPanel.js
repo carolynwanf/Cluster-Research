3e5533141a1a440b46fd1f415d297a1229bd7ef6
Carolyn Wang <carolynwang@college.harvard.edu>
2022-11-08 16:20:27 -0500

    Comments for right panel

--- frontend/src/components/RightPanel.js
+++ frontend/src/components/RightPanel.js
@@ -10,11 +10,12 @@ export const RightPanel = ({ points }) => {
   const [selectedItems, setSelectedItems] = useState([]);
   const [countsArray, setCountsArray] = useState([]);
 
-  const generateTokens = (currCountsArray) => {
+  // Generates max of 5 new representative labels
+  const generateLabels = (currCountsArray) => {
     let newSelectedItems = [];
     let randomIndices = new Set();
 
-    // Adds random indices
+    // Generates up to 5 random indices
     if (currCountsArray.length > 5) {
       while (randomIndices.size < 5) {
         randomIndices.add(Math.floor(Math.random() * currCountsArray.length));
@@ -23,6 +24,7 @@ export const RightPanel = ({ points }) => {
       for (let i = 0; i < currCountsArray.length; i++) randomIndices.add(i);
     }
 
+    // Adds corresponding items to selected items
     randomIndices.forEach((i) => {
       newSelectedItems.push(
         <tr key={currCountsArray[i][1].id} onClick={(e) => highlightToken(e)}>
@@ -37,6 +39,7 @@ export const RightPanel = ({ points }) => {
     setSelectedItems(newSelectedItems);
   };
 
+  // Generates table items if there are selected points
   if (points.length > 0 && selectedItems.length === 0) {
     let counts = {};
 
@@ -53,37 +56,37 @@ export const RightPanel = ({ points }) => {
 
     setCountsArray(newCountsArray);
 
-    generateTokens(newCountsArray);
+    generateLabels(newCountsArray);
   } // Update selected items if selection is cleared
   else if (points.length === 0 && selectedItems.length > 0) {
     setSelectedItems([]);
   }
 
-  // Generates new tokens on refresh
-  const handleRefreshTokens = (e) => {
-    generateTokens(countsArray);
+  // Generates new labels on refresh
+  const handleRefreshLabels = (e) => {
+    generateLabels(countsArray);
   };
 
   return (
     <div className="right panel">
-      <p className="title">Representative tokens</p>
-      <p>{countsArray.length} total unique tokens</p>
+      <p className="title">Representative labels</p>
       <Table bordered>
         <thead>
           <tr>
             <th>#</th>
-            <th>token</th>
+            <th>label</th>
           </tr>
         </thead>
         <tbody>{selectedItems}</tbody>
       </Table>
-      {/* TODO: add button to refresh representative tokens*/}
+      <p>({countsArray.length} total unique labels)</p>
       <Button
-        id="refreshTokensButton"
+        id="refreshLabelsButton"
         variant="secondary"
         onClick={(e) => {
-          handleRefreshTokens(e);
+          handleRefreshLabels(e);
         }}
+        disabled={selectedItems.length < 5 ? true : false}
       >
         new sentences
       </Button>
